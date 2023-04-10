ddc91c58e858fec882d614f0aceb5d4371a6475a
Isaias Santos <isaiassousa032@gmail.com>
2023-04-10 15:32:21 -0300

    Front End RocketNotes

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import { RiShutDownLine } from 'react-icons/ri';
+import { useAuth } from '../../hooks/auth';
+import { Container, Profile, Logout } from "./styles";
+import { api } from '../../../../api/src/services/api'
+
+export function Header() {
+    const { signOut, user } = useAuth();
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+    return (
+        <Container>
+            <Profile to="/profile">
+                <img
+                    src={avatarUrl}
+                    alt={user.name}
+                />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>{user.name}</strong>
+                </div>
+            </Profile>
+
+            <Logout onClick={signOut}>
+                <RiShutDownLine />
+            </Logout>
+        </Container>
+    );
+}
